8112153743dd4fa8d9eec74cde21a6027517c4da
Flávio Rocha <flavio-10_28@hotmail.com>
2021-12-31 18:26:19 -0300

    Criação de loop para a tentativa de entrada de dados do usuário

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -3,19 +3,28 @@ print("Bem-vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = 42
+total_de_tentativas = 3
+rodada = 1
+total_radada = total_de_tentativas
 
-chute = input("Digite o seu número: ")
+while total_de_tentativas > 0:
 
-print("Você digitou", chute)
+    print("Tentativa", rodada, "de", total_radada)
+    chute = input("Digite o seu número: ")
+    print("Você digitou", chute)
+    chute = int(chute)
 
-chute = int(chute)
+    if numero_secreto == chute:
+        print("Você acertou :) ")
+        break
+    elif chute > numero_secreto:
+            print("Você errou! O seu chute foi maior do que o número secreto. :( ")
+            total_de_tentativas = total_de_tentativas - 1
+    elif chute < numero_secreto:
+        print("Você errou! O seu chute foi menor do que o número secreto. :( ")
+        total_de_tentativas = total_de_tentativas - 1
 
-if numero_secreto == chute:
-    print("Você acertou :) ")
-elif chute > numero_secreto:
-        print("Você errou! O seu chute foi maior do que o número secreto. :( ")
-elif chute < numero_secreto:
-    print("Você errou! O seu chute foi menor do que o número secreto. :( ")
+    rodada = rodada + 1
 
 print("Fim do jogo")
 
