a3e966205003790940f39ce97d5e6bdb7e28a9f3
Flávio Rocha <flavio-10_28@hotmail.com>
2021-12-31 17:51:57 -0300

    Entrada de dados e comparação de valores

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,1 +1,14 @@
-print("Bem vindo no jogo de Adivinhação!")
+print("*********************************")
+print("Bem-vindo no jogo de Adivinhação!")
+print("*********************************")
+
+numero_secreto = 42
+
+chute = input("Digite o seu número: ")
+
+print("Você digitou", chute)
+
+if numero_secreto == int(chute):
+    print("Você acertou :) ")
+else:
+    print("Você errou :( ")
