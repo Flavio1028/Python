6880946c1b7bc734bc621737f4b99cffe752ab1f
Flávio Rocha <flavio-10_28@hotmail.com>
2022-01-01 10:33:38 -0300

    Criação da seleção do nível do jogo

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -5,7 +5,22 @@ print("Bem-vindo no jogo de Adivinhação!")
 print("*********************************")
 
 numero_secreto = random.randrange(1, 101)
-total_de_tentativas = 3
+total_de_tentativas = 0
+
+print("Qual nível de dificuldade?")
+print("(1) Fácil (2) Médio (3) Difícil")
+
+nivel = int(input("Defina o nível"))
+
+if nivel == 1:
+    total_de_tentativas = 20
+elif nivel == 2:
+    total_de_tentativas = 10
+elif nivel == 3:
+    total_de_tentativas = 5
+else:
+    total_de_tentativas = 3
+
 rodada = 1
 total_radada = total_de_tentativas
 
