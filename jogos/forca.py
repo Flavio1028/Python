e3f48fa49a4b32c023bf260db32acef6b6182844
Flávio Rocha <flavio-10_28@hotmail.com>
2022-01-06 18:23:21 -0300

    Melhorias no jogo

--- jogos/forca.py
+++ jogos/forca.py
@@ -4,23 +4,38 @@ def jogar_forca():
     print("***Bem vindo no jogo de forca!***")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    palavra_secreta = "maça".upper()
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while not enforcou and not acertou:
 
         chute = input("Qual letra ? ")
-        chute = chute.strip()
+        chute = chute.strip().upper()
 
-        index = 0
-        for letra in palavra_secreta:
-            if chute.upper() == letra.upper():
-                print("Encontrei a letra {} na posição {}".format(letra, index))
-            index = index + 1
+        if chute in palavra_secreta:
+            index = 0
+            for letra in palavra_secreta:
+                if chute == letra:
+                    letras_acertadas[index] = letra
 
-        print("Jogando.....")
+                index += 1
+        else:
+            erros += 1
+
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
+
+    if acertou:
+        print("Você ganhou !")
+    else:
+        print("Você perdeu !")
 
     print("Fim do jogo")
 
