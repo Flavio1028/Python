1ed9091232d997def5ae6c1059395024f64ec32c
Flávio Rocha <flavio-10_28@hotmail.com>
2022-01-09 13:27:07 -0300

    Melhorias na classa Conta

--- oo/main.py
+++ oo/main.py
@@ -1,5 +1,9 @@
 from conta import Conta
 
 conta = Conta(123, "Nico", 55.5, 1000.0)
+conta2 = Conta(321, "Marco", 100.0, 1000.0)
 
-print(conta)
+conta2.transfere(10.0, conta)
+
+print(conta.extrato())
+print(conta2.extrato())
