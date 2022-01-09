ea2c4452212f20582d17ae0101347ef4798c0b67
Flávio Rocha <flavio-10_28@hotmail.com>
2022-01-09 15:34:19 -0300

    Criação de metodos e atributos privados, criação de método estatico

--- oo/conta.py
+++ oo/conta.py
@@ -1,4 +1,3 @@
-
 class Conta:
 
     def __init__(self, numero, titular, saldo, limite):
@@ -13,9 +12,33 @@ class Conta:
     def deposita(self, valor):
         self.__saldo += valor
 
+    def __pode_sacar(self, valor):
+        return valor <= self.__saldo + self.__limite
+
     def saca(self, valor):
-        self.__saldo -= valor
+        if self.__pode_sacar(valor):
+            self.__saldo -= valor
+        else:
+            print("O valor {} passou o limite".format(valor))
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    def get_saldo(self):
+        return self.__saldo
+
+    def get_titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
+
+    @staticmethod
+    def codigo_banco():
+        return "001"
--- oo/main.py
+++ oo/main.py
@@ -1,9 +1,8 @@
 from conta import Conta
 
+print(Conta.codigo_banco())
 conta = Conta(123, "Nico", 55.5, 1000.0)
 conta2 = Conta(321, "Marco", 100.0, 1000.0)
 
-conta2.transfere(10.0, conta)
-
-print(conta.extrato())
-print(conta2.extrato())
+conta.saca(800)
+conta.extrato()
