aa569dbe7120ed4e1eed2cc18818e26f0c02593d
Flávio Rocha <flavio-10_28@hotmail.com>
2022-03-05 11:23:16 -0300

    Validação de numero de telefone Regex

--- python_brasilidades/main.py
+++ python_brasilidades/main.py
@@ -1,5 +1,5 @@
-
 from cpfCnpj import Documento
+from TelefonesBr import TelefonesBr
 
 # 15316264754
 cpf = 15316264754
@@ -11,3 +11,8 @@ cnpj = 61149239000112
 objeto_cnpj = Documento.cria_documento(cnpj)
 
 print(objeto_cnpj)
+
+telefone = "11956481234"
+
+telefone_objeto = TelefonesBr(telefone)
+print(telefone_objeto)
